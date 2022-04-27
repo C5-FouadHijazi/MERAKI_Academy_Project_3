ba4bd5f49c7151bf03ae71b54299c6864c811c1e
FouadHijazi <fouad.o.hijazi@gmail.com>
2022-04-27 03:16:55 +0300

    P2.A] 4.getArticleById

--- controllers/user.js
+++ controllers/user.js
@@ -5,8 +5,11 @@ let userModule = require("../models/userSchema");
 let commentModule = require("../models/commentSchema");
 
 let articleModule = require("../models/articleSchema");
+
 const article = require("../models/articleSchema");
 
+
+//P2.B]1. Register [Level 1]
 const register = (req, res) => {
   const { firstName, lastName, age, country, email, password } = req.body;
   const logInUser = new userModule({
@@ -48,21 +51,22 @@ const register = (req, res) => {
   "author" : "626704538c32c8eac0050ea6"
 } */
 
+
+//P2.A] 1.createNewArticle
 const createNewArticle = (req, res) => {
-  const author = req.quyery.author
-  author.find()
- const logInUser = new userModule({
+  const {title,description, author }= req.body
+ const createarticle = new articleModule({
   title,
   description,
   author,
   });
-  logInUser
+  createarticle
   .save()
   .then((result) => {
     res.status(201).json({
       success: true,
       message: "Article created",
-      author: result.title,
+      author: result,
     });
   })
   .catch((err) => {
@@ -73,6 +77,7 @@ const createNewArticle = (req, res) => {
   });
 }
 
+//P2.A] 2.getAllArticles
 const getAllArticles = (req,res)=>{
   userModule
   .find({})
@@ -93,8 +98,67 @@ const getAllArticles = (req,res)=>{
 }
 
 
+//P2.A] 3.getArticlesByAuthor
+const getArticlesByAuthor = (req,res)=>{
+  const serchauthor = req.query.author
+  articleModule
+  .find({author: serchauthor})
+  .then((result) => {
+    if (result.length !== 0){
+      res.status(404).json({
+        success: false,
+        message: `The author => ${authorId} has no articles`,
+      })
+    }else{
+      res.status(200).json({
+        success: true,
+        message: `All the articles for the author → ${authorId}`,
+        articles : [result]
+      });
+    }
+  
+  })
+  .catch((err) => {
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+      err: "error message"
+    });
+  });
+}
 
 
+
+//P2.A] 4.getArticleById -[Query/Key: id - Value: article id]
+const getArticleById = (req,res)=>{
+  const serchID = req.query.id
+  articleModule
+  .find({ _id : serchID })
+  .then((result) => {
+    console.log(result.length);
+    if (result.length === 0){
+      res.status(404).send({
+        success: false,
+        message: "The article is not found"
+      })
+    }else{
+      console.log("heeeelpppp");
+      res.status(200).send({
+        success: true,
+        message: `The article with id ⇾ ${articlesId}`,
+        articles : result,
+      });
+    }
+  
+  })
+  .catch((err) => {
+    res.status(500).send({
+      success: false,
+      message: "Server Error",
+      err: "error message"
+    });
+  });
+}
 
 
 
@@ -103,4 +167,6 @@ module.exports = {
   register,
   createNewArticle,
   getAllArticles,
+  getArticlesByAuthor,
+  getArticleById,
 }
--- routes/user.js
+++ routes/user.js
@@ -5,12 +5,15 @@ const express = require("express");
 const usersRouter = express.Router()
 const  articelesRouter = express.Router()
 
-const { register, createNewArticle ,getAllArticles} = require("../controllers/user");
+const { register, createNewArticle ,getAllArticles,getArticlesByAuthor,getArticleById} = require("../controllers/user");
 
 
 usersRouter.post("/",register)
 articelesRouter.post("/",createNewArticle)
 articelesRouter.get("/",getAllArticles)
+articelesRouter.get("/search_1",getArticlesByAuthor)
+articelesRouter.get("/search_2",getArticleById)
+
 
 
 module.exports = usersRouter
