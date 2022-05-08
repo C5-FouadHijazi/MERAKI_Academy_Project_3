d943ddfc0581c6e67b56266e3c7c4f41d9ec677a
FouadHijazi <fouad.o.hijazi@gmail.com>
2022-05-08 16:53:03 +0300

    Update Del Add to Articles

--- 4. React-Starter-Code/frontend/src/components/Dashboard/index.js
+++ 4. React-Starter-Code/frontend/src/components/Dashboard/index.js
@@ -5,11 +5,17 @@ import "./style.css";
 import { tokenContext } from "../../App";
 
 const Dashbord = () => {
-const [articles, setArticles]=useState([])
+  const [articles, setArticles] = useState([]);
+  const [update, setUpdate] = useState(false);
+  const [title, setTitle] = useState("");
+
+  const [comment, setComment] = useState("");
+
+  const [description, setDescription] = useState("");
 
   const { token, setToken, islogin, setIslogin, message, setMessage } =
     useContext(tokenContext);
-    setToken(localStorage.getItem("token"));
+  setToken(localStorage.getItem("token"));
 
   useEffect(() => {
     axios
@@ -20,29 +26,118 @@ const [articles, setArticles]=useState([])
       })
       .then((result) => {
         console.log(result);
-        setArticles(result.data.articles)
+        setArticles(result.data.articles);
       })
       .catch((err) => {
         console.log(err);
       });
-  }, []);
-console.log(articles);
+  }, [update]);
+
+  // this function will delete the Article we chose by Id
+
   return (
     <div>
-      {articles && articles.map((element, index) => {
-        return (
-          <div key={element.id}>
-            
-            <p>{element.title}</p>
-            <p>{element.description}</p>
-            <button>delete</button>
-            <br />
-            <button>update</button>
-            <br />
-            <button>Add Comment</button>
-          </div>
-        );
-      })}
+      {articles &&
+        articles.map((element, index) => {
+          return (
+            <div key={element.id}>
+              <p>{element.title}</p>
+              <p>{element.description}</p>
+              {element.comments.map((el) => {
+                return <p>{el.comment}</p>;
+              })}
+              <button
+                className={element._id}
+                onClick={(e) => {
+                  axios
+                    .delete(
+                      `http://localhost:5000/articles/${e.target.className}`,
+                      {
+                        headers: {
+                          authorization: `Bearer ${token}`,
+                        },
+                      }
+                    )
+                    .then((result) => {
+                      console.log(result);
+                      setUpdate(!update);
+                    })
+                    .catch((err) => {
+                      console.log(err);
+                    });
+                }}
+              >
+                x
+              </button>
+              <br />
+              <button
+                className={element._id}
+                onClick={(e) => {
+                  axios
+                    .put(
+                      `http://localhost:5000/articles/${e.target.className}`,
+                      {
+                        title: title,
+                        description: description,
+                      }
+                    )
+                    .then((result) => {
+                      console.log(result);
+                      setUpdate(!update);
+                    })
+                    .catch((err) => {
+                      console.log(err);
+                    });
+                }}
+              >
+                update
+              </button>
+              <input
+                onChange={(e) => {
+                  setTitle(e.target.value);
+                }}
+              />
+              <input
+                onChange={(e) => {
+                  setDescription(e.target.value);
+                }}
+              />
+
+              <br />
+              <button
+                className={element._id}
+                onClick={(e) => {
+                  axios
+                    .post(
+                      `http://localhost:5000/articles/${e.target.className}/comments/`,
+                      {
+                        comment: comment,
+                      },
+                      {
+                        headers: {
+                          authorization: `Bearer ${token}`,
+                        },
+                      }
+                    )
+                    .then((result) => {
+                      console.log(result);
+                      setUpdate(!update);
+                    })
+                    .catch((err) => {
+                      console.log(err);
+                    });
+                }}
+              >
+                Add Comment
+              </button>
+              <input
+                onChange={(e) => {
+                  setComment(e.target.value);
+                }}
+              />
+            </div>
+          );
+        })}
     </div>
   );
 };
