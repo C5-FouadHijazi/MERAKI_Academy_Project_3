76f6fbead580729962ece4ae9f4327be3570f215
FouadHijazi <fouad.o.hijazi@gmail.com>
2022-05-08 20:30:34 +0300

    LogOut

--- 4. React-Starter-Code/frontend/src/components/Navbar/index.js
+++ 4. React-Starter-Code/frontend/src/components/Navbar/index.js
@@ -3,26 +3,34 @@ import { Link, useNavigate } from "react-router-dom";
 import "./style.css";
 import { tokenContext } from "../../App";
 
-
 const Navbar = () => {
-
-  const { islogin } = useContext(tokenContext);
+  const { token, setToken, islogin, setIslogin, message, setMessage } =
+    useContext(tokenContext);
 
   const navigate = useNavigate();
 
+  const logOut = () => {
+    localStorage.removeItem("token");
+    setIslogin(false);
+    setToken("");
+  };
+
   return (
     <div className="Navbar">
-    
       {islogin === true ? (
         <>
           <Link to="/dashbord">Dashboard</Link>
           <Link to="/addnewarticle">Add New Article</Link>
-          <Link to="/Logout">Logout</Link>
-        </> ) : (<>
-        <Link to="/Register/">Register</Link>
-         <Link to="/login">login</Link>
-        </>)}
-
+          <Link to="/login" onClick={logOut}>
+            logout
+          </Link>
+        </>
+      ) : (
+        <>
+          <Link to="/Register/">Register</Link>
+          <Link to="/login">login</Link>
+        </>
+      )}
     </div>
   );
 };
